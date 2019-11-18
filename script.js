1775989f7489917fa1ab58277e358b261c6332c7
jjyoon7 <jay.yoon7@gmail.com>
2019-11-18 22:44:52 +0100

    update addEvenlistner on open nav btn

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 const closeBtn = document.querySelector(".close-nav");
+const openBtn = document.querySelector(".open-nav");
 const nav = document.querySelector(".nav")
 const navOpen = "open-nav"
 
-function handleNav() {
+
+function handleOpenNav() {
     nav.classList.remove(navOpen)
 }
 
+function handleCloseNav() {
+    nav.classList.add(navOpen)
+}
+
 function init() {
-    closeBtn.addEventListener("click", handleNav);
+    closeBtn.addEventListener("click", handleOpenNav);
+    openBtn.addEventListener("click", handleCloseNav);
 }
 
 init();
