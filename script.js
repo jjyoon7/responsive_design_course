9b42a793ae0b4bf1c7d8c890bd5db47cd7a568d9
jjyoon7 <jay.yoon7@gmail.com>
2019-11-19 13:18:05 +0100

    navigation open and close js update

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 const closeBtn = document.querySelector(".close-nav");
 const openBtn = document.querySelector(".open-nav");
-const nav = document.querySelector(".nav")
-const navOpen = "open-nav"
+const nav = document.querySelector(".nav");
+const navOpen = "open-navigation";
 
 
 function handleOpenNav() {
-    nav.classList.remove(navOpen)
-}
-
-function handleCloseNav() {
     nav.classList.add(navOpen)
 }
 
+function handleCloseNav() {
+    nav.classList.remove(navOpen)
+}
+
 function init() {
-    closeBtn.addEventListener("click", handleOpenNav);
-    openBtn.addEventListener("click", handleCloseNav);
+    closeBtn.addEventListener("click", handleCloseNav);
+    openBtn.addEventListener("click", handleOpenNav);
 }
 
 init();
